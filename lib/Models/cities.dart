d1a8812b9b4fc14a807c144baafaced2ab546cbb
Michal <michaldabek1990@gmail.com>
2022-05-12 14:37:00 +0200

    Wyszukiwanie miast z pliku jsona

---
+++ lib/Models/cities.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+class Cities {
+  num id;
+  String name;
+  String country;
+  String state;
+
+  Cities({required this.name, required this.country, required this.state, required this.id});
+
+
+  factory Cities.fromJson(Map<String, dynamic> json) {
+    return Cities(
+      id: json['id'],
+      name: json['name'],
+      country: json['country'],
+      state: json['state'],
+    );
+  }
+
+  static Future<List<Cities>> fetchCities(String cityName) async {
+
+    final response = await rootBundle.loadString('lib/assets/city_list.json');
+    final List<dynamic> data = await json.decode(response);
+    print(cityName);
+    List<Cities> selectedCity = List<Cities>.from(data.map((e) => Cities.fromJson(e)).where((city) => city.name.contains(cityName)));
+
+    return selectedCity;
+
+    // final response = await http.get(Uri.parse('http://api.openweathermap.org/geo/1.0/direct?q='+cityName+'&limit=4&appid=818eb6d59452047d1ffbdb20e1b9ff83'),
+    //   headers: <String, String>{
+    //     'Content-Type': 'application/json; charset=UTF-8',
+    //   },);
+    //
+    // if (response.statusCode == 200) {
+    //
+    //   List<dynamic> parsedListJson = jsonDecode(response.body);
+    //   List<Cities> selectedCity = List<Cities>.from(parsedListJson.map((i) => Cities.fromJson(i)));
+    //
+    //   print(selectedCity[0].name);
+    //   return selectedCity;
+    // } else {
+    //   // If the server did not return a 200 OK response,
+    //   // then throw an exception.
+    //   throw Exception('Failed to load cities');
+    // }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'Models/cities.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,27 +17,50 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SimpleWeather(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class SimpleWeather extends StatefulWidget {
+  const SimpleWeather({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SimpleWeather> createState() => _SimpleWeatherState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SimpleWeatherState extends State<SimpleWeather> {
+  List<Cities>? selectedCity;
+ bool isReady = false;
 
-  void _incrementCounter() {
+
+  @override
+  void initState() {
+    super.initState();
+     fillData('Lubin');
+    print('initstate');
+
+  }
+
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    myController.dispose();
+    super.dispose();
+  }
+
+  fillData(String city) async {
+    selectedCity = await Cities.fetchCities(city);
     setState(() {
-      _counter++;
+      isReady = true;
     });
+
+    print(selectedCity?[0].name);
   }
 
   @override
@@ -44,22 +71,73 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              width: 300,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+                child: TextField(
+                  obscureText: false,
+                  textCapitalization: TextCapitalization.words,
+                  controller: myController,
+                  decoration: InputDecoration(hintText: 'Wpisz szukane miasto'),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            MaterialButton(
+                onPressed: () {
+                  setState(() {
+                    fillData(myController.text);
+                  });
+                },
+                child: Text("Sprawdź"),
+                color: Colors.indigoAccent),
+            if(isReady)
+            Container(
+              height: 600,
+              child: ListView.builder(
+                  itemCount: selectedCity!.length,
+                  itemBuilder:  (BuildContext context, int i){
+                    final city = selectedCity![i];
+                    return
+                        Card(
+                          elevation: 5,
+                          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(city.id.toString()),
+                                ),
+                                Column(children: [
+                                  Text(city.name,
+                                  style: Theme.of(context).textTheme.headline4,),
+                                  Text(city.state),
+                                  Text(city.country),
+
+
+                                ],),
+                              ],
+                            ),
+                          ),
+                        );
+                  }
+              ),
             ),
+            // Text(
+            //   name,
+            //   style: Theme.of(context).textTheme.headline5,
+            // ),
+            // //  Text(selectedCity[0].country != null ? "Brak":"tada",
+            //
+            //     style: Theme.of(context).textTheme.headline5,
+            //   ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
